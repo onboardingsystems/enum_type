583fde8f1846d98694c6cf2f7946acc1d0df18d0
Lok Chun Wai <cwlok0613@gmail.com>
2023-07-17 17:08:35 +0800

    Fix export formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,13 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  import: [:ecto],
+  import_deps: [:ecto],
   locals_without_parens: [
     defenum: :*,
     value: :*,
-    default: 1,
+    default: 1
+  ],
+  export: [
+    locals_without_parens: [value: :*, default: 1]
   ]
 ]
